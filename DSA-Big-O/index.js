025b1cc5a280462ee41da692b45745f71cba0c5a
hunterwaters <55896961+hunterwaters@users.noreply.github.com>
2020-05-19 13:51:26 -0700

    Add files via upload

---
+++ DSA-Big-O/index.js
@@ -0,0 +1,38 @@
+
+/*
+
+BIG O NOTATION
+ 
+ 1. Constant time 0(1)
+ Logarithmic O(log(n))
+
+ 2. Constant time 0(1)
+ This would be constant time because no matter the sze of the input, the algorithm will take the same amount of time to complete
+
+ 3. Polynomial time O(n^k)2 
+ This would classify as polynomial time because this problem requires 2 levels of looping over an input using nested loops.
+
+ 4. Linear time O(n)
+ We would use linear time here because the running time if directly proportional to the size of the input, if the array is larger, it will take a longer time to double the size of the algorithm
+
+ 5. Linear time O(n)
+ This woule be linear time also because we are comparing the array[i] to the item until they equal each other, the run time is allotted for how long the array is that it is being compared to
+
+ 6. Polynomial time O(n^k)2
+ This would require us to use poylnomial time because it is handling the algorithm in nested loops
+
+ 7.Exponential Time O(2^n)
+  This algorithm uses exponential time and is computing the fibonacci sequence, increases and doubles the time exponentially from each input given to the algorithm
+
+8. Constant time O(1)
+This algorithm is using constant time because it is only completing one equation at a time, given the input it is divided by two and then the math is done within the equation itself
+
+9.Constant time O(1)
+The equation returns a random element in the array no matter the size of the array, for this it is using constant time to determine a random element in the array
+
+10.Constant time O(1)
+This algorithm determines whether a number is prime or not, if the number is prime it returns true, if the number is not prime it returns false
+It uses constant time because no matter what number is being inputted into the equation, the equation will have to evaluate an equal number of steps to determine if it is prime or not.
+
+
+*/
